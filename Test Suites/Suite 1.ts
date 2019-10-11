e48f05efc857acd94c32b7496dbd4f0bce63a70b
udayt139 <uday@totient.co.in>
2019-10-11 20:59:50 +0530

    3

--- Test Suites/Suite 1.ts
+++ Test Suites/Suite 1.ts
@@ -14,24 +14,24 @@
       <guid>59912a87-b69b-42d7-9ea6-f17ff34db0ba</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Create User</testCaseId>
+      <testCaseId>Test Cases/Sample/Create User</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>55777806-9861-4e93-a1e5-7d7032feab10</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Delete User</testCaseId>
+      <testCaseId>Test Cases/Sample/Delete User</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>3d925826-d966-4f6c-a11a-02d16974d88d</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Get Single User</testCaseId>
+      <testCaseId>Test Cases/Sample/Get Single User</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>7b238039-8ee1-46a7-85ff-95c042e9c7ac</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Update User</testCaseId>
+      <testCaseId>Test Cases/Sample/Update User</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
